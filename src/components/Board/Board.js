80f5ec2b73b661cfc3752a04dabac6f129cfa616
Matthew Chao <matthew.chao@techtonic.com>
2020-11-02 10:12:52 -0500

    run prettier

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,18 +1,13 @@
-import React from 'react'
-import './Board.css';
+import React from "react";
+import "./Board.css";
 
 const Board = (props) => {
-
   const gridConfig = {
     gridTemplateColumns: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
     gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
-  }
+  };
 
+  return <div className="Board" style={gridConfig}></div>;
+};
 
-  return (
-    <div className='Board' style={gridConfig}>
-    </div>
-  )
-}
-
-export default Board
+export default Board;
