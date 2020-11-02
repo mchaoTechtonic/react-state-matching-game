d50813e244674fd2e8d8c0fb770686950d8aa8a8
mchaoTechtonic <matthew.chao@techtonic.com>
2020-11-02 16:44:36 -0500

    pass tests 2.11-13; for 2.13 use the distinct notation from html https://reactjs.org/docs/handling-events.html

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -30,6 +30,7 @@ class App extends Component {
         <OptionsPanel
           playing={this.state.playing}
           numTiles={this.state.numTiles}
+          startGame={this.startGame}
         />
         <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
       </div>
