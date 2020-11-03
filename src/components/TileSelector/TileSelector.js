a5eb8ff2d426cdd6dae6cc2e010c75d5535557ec
mchaoTechtonic <matthew.chao@techtonic.com>
2020-11-03 16:00:37 -0500

    pass tests 4.16-17; had to make sure to use array destructuring instead of object destructuring to assign from useHover

--- src/components/TileSelector/TileSelector.js
+++ src/components/TileSelector/TileSelector.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./TileSelector.css";
+import useHover from '../../hooks';
 
 const TileSelector = (props) => {
-  const dropdown = (
+  const [ref, hovered] = useHover();
+  const dropdown = hovered ? (
     <div className="tileSelectorContent">
       <div className="number">4</div>
       <div className="number">16</div>
       <div className="number">36</div>
     </div>
-  );
+  ) : null;
 
   return (
     <div className="tileSelector">
       <div>Number of Tiles</div>
-      <div className="tileSelectorDropdown">
+      <div className="tileSelectorDropdown" ref={ref}>
         {props.numTiles}
         {dropdown}
       </div>
