e16f37f494937847e7799c4ae3a7c23238b79036
mchaoTechtonic <matthew.chao@techtonic.com>
2020-11-03 15:43:31 -0500

    pass tests 4.9-10

--- src/hooks.js
+++ src/hooks.js
@@ -3,10 +3,16 @@ import { useEffect, useRef, useState } from 'react';
 function useHover() {
     const ref = useRef();
     const [hovered, setHovered] = useState(false);
+    function enter() {
+        setHovered(true);
+    }
+    function leave() {
+        setHovered(false);
+    }
     useEffect(() => {
         const refCopy = ref;
-        refCopy.current.addEventListener('mouseenter', function () { });
-        refCopy.current.addEventListener('mouseleave', function () { });
+        refCopy.current.addEventListener('mouseenter', enter);
+        refCopy.current.addEventListener('mouseleave', leave);
         return function () { };
     })
     return [ref, hovered];
