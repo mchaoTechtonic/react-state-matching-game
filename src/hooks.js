9adb48971c354ca920bd0b57a62e619eed6a9d56
mchaoTechtonic <matthew.chao@techtonic.com>
2020-11-03 15:47:17 -0500

    pass tests 4.11-12

--- src/hooks.js
+++ src/hooks.js
@@ -13,7 +13,12 @@ function useHover() {
         const refCopy = ref;
         refCopy.current.addEventListener('mouseenter', enter);
         refCopy.current.addEventListener('mouseleave', leave);
-        return function () { };
+        // if useEffect's argument returns a function f
+        // then f is used as 'cleanup' https://reactjs.org/docs/hooks-reference.html#cleaning-up-an-effect
+        return function () {
+            refCopy.current.removeEventListener('mouseenter', enter);
+            refCopy.current.removeEventListener('mouseleave', leave);
+        };
     })
     return [ref, hovered];
 }
